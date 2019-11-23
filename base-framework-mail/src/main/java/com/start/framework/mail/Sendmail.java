d6ff96b159b435f450b37912fd35053e22ce6d8b
hsn999 <h_sn999@126.com>
2019-11-23 17:09:50 +0800

    update

--- base-framework-mail/src/main/java/com/start/framework/mail/Sendmail.java
+++ base-framework-mail/src/main/java/com/start/framework/mail/Sendmail.java
@@ -17,8 +17,8 @@ public class Sendmail {
 	public void sendTxtMail() {
 		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
 		// 设置收件人，寄件人
-		simpleMailMessage.setTo(new String[] { "miles02@163.com" });
-		simpleMailMessage.setFrom("miles02@163.com");
+		simpleMailMessage.setTo(new String[] { "xxx@126.com" });
+		simpleMailMessage.setFrom("fhhe@126.com");
 		simpleMailMessage.setSubject("Spring Boot Mail 邮件测试【文本】");
 		simpleMailMessage.setText("这里是一段简单文本。");
 		// 发送邮件
@@ -35,8 +35,8 @@ public class Sendmail {
 	public void sendHtmlMail() throws Exception {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-		mimeMessageHelper.setTo("miles02@163.com");
-		mimeMessageHelper.setFrom("miles02@163.com");
+		mimeMessageHelper.setTo("xxx@126.com");
+		mimeMessageHelper.setFrom("xx1x@126.com");
 		mimeMessageHelper.setSubject("Spring Boot Mail 邮件测试【HTML】");
 
 		StringBuilder sb = new StringBuilder();
@@ -63,8 +63,8 @@ public class Sendmail {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 		// multipart模式
 		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-		mimeMessageHelper.setTo("miles02@163.com");
-		mimeMessageHelper.setFrom("miles02@163.com");
+		mimeMessageHelper.setTo("xxx@126.com");
+		mimeMessageHelper.setFrom("xxx1@126.com");
 		mimeMessageHelper.setSubject("Spring Boot Mail 邮件测试【图片】");
 
 		StringBuilder sb = new StringBuilder();
@@ -78,7 +78,7 @@ public class Sendmail {
 		mimeMessageHelper.setText(sb.toString(), true);
 
 		// 设置imageId
-		FileSystemResource img = new FileSystemResource(new File("E:/1.jpg"));
+		FileSystemResource img = new FileSystemResource(new File("E:/new.jpg"));
 		mimeMessageHelper.addInline("imageId", img);
 
 		// 发送邮件
@@ -96,8 +96,8 @@ public class Sendmail {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 		// multipart模式
 		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "utf-8");
-		mimeMessageHelper.setTo("miles02@163.com");
-		mimeMessageHelper.setFrom("miles02@163.com");
+		mimeMessageHelper.setTo("xxx@126.com");
+		mimeMessageHelper.setFrom("xxx1@126.com");
 		mimeMessageHelper.setSubject("Spring Boot Mail 邮件测试【附件】");
 
 		StringBuilder sb = new StringBuilder();
@@ -108,7 +108,7 @@ public class Sendmail {
 		// 启用html
 		mimeMessageHelper.setText(sb.toString(), true);
 		// 设置附件
-		FileSystemResource img = new FileSystemResource(new File("E:/1.jpg"));
+		FileSystemResource img = new FileSystemResource(new File("E:/new.jpg"));
 		mimeMessageHelper.addAttachment("image.jpg", img);
 
 		// 发送邮件
